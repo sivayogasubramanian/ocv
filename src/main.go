0f438c22a7166aec8620b9fba62a5fb200b4ca9b
Siva <sivayogasubramanian@gmail.com>
2023-09-23 19:35:07 +0800

    Fix .env error on production

--- src/main.go
+++ src/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sivayogasubramanian/ocv/src/config"
 	"github.com/sivayogasubramanian/ocv/src/routes"
 	"log"
+	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
 	}
 
 	config.InitDB()
